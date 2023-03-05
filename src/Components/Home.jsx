885f82bf7105cacedc348071212d6156311ac9bc
Shubh Agrawal <shubhagrawal1280@gmail.com>
2023-03-05 11:13:43 +0530

    login implementation

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,26 +1,54 @@
 import React, { useState, useEffect } from "react";
 import MovieComponent from "./MovieComponent";
+import Loader from "./Loader";
 
 const Home = () => {
   const [card, setCard] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
 
   const getCardData = async () => {
     const res = await fetch(
-      `https://jsonplaceholder.typicode.com/posts?_limit=9&_page=1`
+      `https://jsonplaceholder.typicode.com/posts?_limit=9&_page=${page}`
     );
-
     const data = await res.json();
-    console.log(data);
-    setCard(data);
+
+    //solved: the first page is repeated twice
+    if (page !== 1) {
+      setCard((prev) => [...prev, ...data]);
+    } else {
+      setCard([...data]);
+    }
+    setLoading(false);
   };
 
   useEffect(() => {
     getCardData();
+  }, [page]);
+
+  const handleInfiniteScroll = async () => {
+    try {
+      if (
+        window.innerHeight + window.document.documentElement.scrollTop + 1 >=
+        window.document.documentElement.scrollHeight
+      ) {
+        setLoading(true);
+        setPage((prev) => prev + 1);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleInfiniteScroll);
+    return () => window.removeEventListener("scroll", handleInfiniteScroll);
   }, []);
 
   return (
     <>
       <MovieComponent movieInfo={card} />
+      {loading && <Loader />}
     </>
   );
 };
---
+++ src/Components/Loader.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1>Loading..</h1>
+    </div>
+  );
+};
+
+export default Loader;
